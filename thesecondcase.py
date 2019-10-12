c7af268b243fa819988813aa52c43d23af7f7632
AlinaDaniel <alina.daniel66@icloud.com>
2019-10-12 17:46:57 +0300

    Task allocation.

--- thesecondcase.py
+++ thesecondcase.py
@@ -1,3 +1,44 @@
 # Case #2.
-#
+# Developers:   Zemtseva A. (%),
+#               Zaitseva A. (%),
+#               Daniel A.   (%).
 
+
+# TODO (Zaitseva A): Circle function for calculating a annual income.
+
+pass
+
+
+# TODO (Daniel A): The user chooses which category of the population he belongs to.
+
+pass
+
+
+# TODO (Daniel A): Function for one subject.
+
+pass
+
+
+# TODO (Zemtseva A): Function for a married couple.
+
+pass
+
+
+# TODO (Zaitseva A): Function for a single parent.
+
+pass
+
+
+# TODO (Zemtseva A): Circle function for calculating an amount of the annual tax.
+
+pass
+
+
+# TODO (Daniel A): Localization for the case.
+
+pass
+
+
+# TODO (Daniel A): Code rewiew.
+
+pass
